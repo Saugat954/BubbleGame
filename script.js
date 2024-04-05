0c2074ec835663f4593b2a41ed5b8f6e5e556eb4
Saugat954 <156522705+Saugat954@users.noreply.github.com>
2024-04-05 14:14:40 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+var timer = 30;
+var score = 0;
+var hitrn=0;
+
+
+
+
+
+
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit() {
+  hitrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hitrn;
+
+}
+
+
+function makeBubble() {
+  var clutter = "";
+
+
+  for (var i = 1; i <= 80; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += ` <div class="bubble">${rn}</div>`;
+  }
+  document.querySelector('#pbtm').innerHTML = clutter;
+}
+
+
+function runTimer() {
+  var timerinterval = setInterval(function () {
+    if (timer > 0) {
+
+      timer--;
+      document.querySelector('#timerval').textContent = timer;
+    }
+    else {
+      clearInterval(timerinterval);
+      document.querySelector("#pbtm").innerHTML="";
+    }
+  }, 1000)
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  var clickedNum = Number(dets.target.textContent);
+  if(clickedNum===hitrn){
+    increaseScore();
+    makeBubble();
+    getNewHit();
+  }
+
+});
+
+runTimer();
+makeBubble();
+getNewHit();
+// increaseScore();
